d2458d7b7682aef5a2fbb04987f5e0464fc30fa2
LopesManuel <lpsmanuel@gmail.com>
2015-12-06 18:10:07 +0000

    Initial Commit

---
+++ 243-[Intermediate]-Jennys-Fruit-Basket/jfb.lua
@@ -0,0 +1,62 @@
+--Main
+local fruits = {}
+local wallet = 500
+local max = 0
+--Read input
+while true do
+  local line = io.read()
+  if line == nil then break end
+  local n  = string.match(line, "%a+")
+  local p  = string.match(line, "%d+")
+  local fruit = {name = n,price = p, howMany = 0, max = math.floor(wallet/p)}
+  if math.floor(wallet/p) > max then max = math.floor(wallet/p) end
+  table.insert(fruits, fruit)
+end
+
+function printFruits() do
+	local output = ""
+	for j, fruit in ipairs(fruits) do
+		if fruit.howMany > 0 and j < #fruits and j > 0 and output ~= "" then
+			output = output .. ","
+		end
+		if fruit.howMany > 1 then 
+			output = output .. fruit.howMany .." ".. fruit.name .. "s" 
+		elseif fruit.howMany > 0 then
+			output = output .. fruit.howMany .." ".. fruit.name
+		end
+	end
+	print(output)
+end
+end
+
+function calculateWallet(wall, f) do
+	for j, fruit in ipairs(fruits) do 
+		fruit.howMany = f[j] or 0
+		wall = wall - fruit.howMany*fruit.price
+	end
+	if wall == 0 then 
+		printFruits() 
+	end
+end
+end
+
+function Combinations(janne,max, wll)
+  local a = {}
+  local vals = janne
+  local n = max
+
+  local function aux(m, w)
+    for i = 0, janne[m].max do
+		a[m] = i
+		if m < #janne then
+			aux(m + 1, w)      
+	     else
+			calculateWallet(w, a)
+	      end		
+	  end
+  end
+
+  aux(1,wll)
+end
+
+Combinations(fruits,max, wallet)
