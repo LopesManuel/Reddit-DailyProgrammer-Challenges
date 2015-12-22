fe0725ed98bafcb2845dbc458845a0beb53ee76c
LopesManuel <lpsmanuel@gmail.com>
2015-12-22 12:00:17 +0000

    Done

--- 245-[Intermediate]-Ggggggg-gggg-Ggggg-ggggg!/decoder.lua
+++ 245-[Intermediate]-Ggggggg-gggg-Ggggg-ggggg!/decoder.lua
@@ -10,7 +10,6 @@ for match in line:gmatch("%a+ %a+")do
   local key  = string.match(match, "%a+")
   local value  = string.match(match, "%a+$")
   local entry = {k = key, v = value}
-      print( key .. " " .. value)
   table.insert(dictionary, entry)
 end
 --Second line has the message
@@ -18,11 +17,14 @@ line = io.read()
 if line == nil then print("Input error!") return end
 --Decode
 local output = ""
-for match,p in line:gmatch("%a%a%a")do
-  print(match)
-  for j, entry in ipairs(dictionary) do
-    match = string.gsub(match, entry.v, entry.k)
+for word in line:gmatch("%w+")do
+  local outWord = ""
+  for match in word:gmatch("%a%a%a")do
+    for j, entry in ipairs(dictionary) do
+      match = string.gsub(match, entry.v, entry.k)
+    end
+    outWord = outWord .. match
   end
-  output = output .. match
+  line = string.gsub(line, word, outWord)
 end
-print(output)
+print(line)
